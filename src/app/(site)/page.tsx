02ab43dc3e0f934aea355e99b0b0b0486dde9945
Michael Dos Santos <michael.dozzantoz@gmail.com>
2025-03-19 20:33:34 +0000

    new landing page layout change with image

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,53 +4,61 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <Section>
-      <div className="w-full md:mt-24 md:mb-48 max-w-6xl mx-auto p-4 md:p-8">
-        <Component centeredmobile small padding>
-          <h1>Curated Health & Fitness Platform</h1>
-          <p className="max-w-md">
-            A hand-picked directory of health & fitness resources and custom
-            tools.
-          </p>
-          <div className="flex flex-row gap-2 items-center justify-center">
+    <>
+      <Section
+        full
+        className="relative -mt-[var(--header-height-mobile)] md:-mt-[var(--header-height-desktop)]"
+      >
+        <Image
+          src="/manthing.png"
+          alt="Verzes Manthing"
+          width={1200}
+          height={600}
+          priority
+          className="w-full object-cover -mt-6 md:mt-0  mx-auto aspect-[4/5] md:aspect-[21/9] mix-blend-multiply greyscale-0 saturate-0 opacity-50 "
+        />
+        <div className="w-full absolute bottom-0 left-0 z-10 h-full flex flex-col  items-center justify-center top-to-bottom-gradient">
+          <Row>
+            <Component centered small padding>
+              <h1>Curated Health & Fitness Platform</h1>
+              <p className="max-w-md">
+                A hand-picked directory of health & fitness resources and custom
+                tools.
+              </p>
+              <div className="flex flex-row gap-2 items-center justify-center">
+                <Link
+                  href="/workout-generator"
+                  className="bg-black text-white text-sm md:text-base  rounded-full px-6 py-2 hover:bg-black/80"
+                >
+                  Workout Generator {"->"}
+                </Link>
+              </div>
+            </Component>
+          </Row>
+        </div>
+      </Section>
+      <Section>
+        <Row>
+          <Component centered>
+            <h2>Health & Fitness Articles</h2>
+
+            <p className="px-6 py-2 bg-black/5 rounded-full w-fit mx-auto">
+              Coming soon...
+            </p>
+          </Component>
+          <Component small centered>
+            <h2>Workout Generator</h2>
+
+            <p>Generate bespoke workouts with a simple prompt.</p>
             <Link
               href="/workout-generator"
-              className="bg-black/5 text-sm md:text-base  rounded-full px-6 py-2 hover:bg-black/10"
+              className="bg-black text-sm w-fit md:text-base text-background rounded-full px-6 py-2 hover:bg-black/80"
             >
-              Workout Generator {"->"}
+              Try for free {"->"}
             </Link>
-          </div>
-        </Component>
-      </div>
-
-      <Image
-        src="/manthing.jpg"
-        alt="Verzes Manthing"
-        width={1200}
-        height={400}
-        priority
-        className="md:absolute w-full md:top-0 md:-z-10 md:right-0 max-w-7xl  object-cover -mt-6 md:mt-0  mx-auto aspect-[4/3] md:aspect-[16/9] mix-blend-luminosity brightness-105 greyscale-0 opacity-100 md:-scale-x-100"
-      />
-      <Row>
-        <Component centered>
-          <h2>Health & Fitness Articles</h2>
-
-          <p className="px-6 py-2 bg-black/5 rounded-full w-fit mx-auto">
-            Coming soon...
-          </p>
-        </Component>
-        <Component small centered>
-          <h2>Workout Generator</h2>
-
-          <p>Generate bespoke workouts with a simple prompt.</p>
-          <Link
-            href="/workout-generator"
-            className="bg-black text-sm w-fit md:text-base text-background rounded-full px-6 py-2 hover:bg-black/80"
-          >
-            Try for free {"->"}
-          </Link>
-        </Component>
-      </Row>
-    </Section>
+          </Component>
+        </Row>
+      </Section>
+    </>
   );
 }
--- src/components/global/ui/section.tsx
+++ src/components/global/ui/section.tsx
@@ -2,13 +2,19 @@ export default function Section({
   children,
   full,
   grey,
-}: Readonly<{ children: React.ReactNode; full?: boolean; grey?: boolean }>) {
+  className,
+}: Readonly<{
+  children: React.ReactNode;
+  full?: boolean;
+  grey?: boolean;
+  className?: string;
+}>) {
   return (
     <div
       className={`flex flex-col gap-8 w-full  relative   ${
-        full ? "mt-[-52px] md:mt-[-80px]" : " md:py-4 "
+        full ? "" : " md:py-4 "
       }
-       ${grey ? "bg-secondary pt-6 md:pt-12 mb-6" : "p-0"}  `}
+       ${grey ? "bg-secondary pt-6 md:pt-12 mb-6" : "p-0"}  ${className}`}
     >
       {children}
     </div>
